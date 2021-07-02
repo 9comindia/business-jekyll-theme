a808271311f09aed238b4c4294f862e8477ef919
9comindia <67412987+9comindia@users.noreply.github.com>
2021-07-03 00:17:06 +0530

    Update directorySlider.js

--- js/directorySlider.js
+++ js/directorySlider.js
@@ -44,7 +44,7 @@ $.ajax({
     success: function (data) {
         $(data).find("a").attr("href", function (i, val) {
             if( val.match(/\.(jpe?g|png|gif)$/) ) { 
-                $("body").append( "<img src='"+ folder + val +"'>" );
+                $("body").append( "<img src='"+ folder + val +"' />" );
             } 
         });
     }
