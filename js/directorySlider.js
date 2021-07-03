50b85d9cfd1c663fb90bcc06c93c328d5cf569c1
9comindia <67412987+9comindia@users.noreply.github.com>
2021-07-03 13:08:43 +0530

    Update directorySlider.js

--- js/directorySlider.js
+++ js/directorySlider.js
@@ -6,6 +6,14 @@
  * License: GPL v3
  */
 
+/**
+ * directorySlider v0.9 - Loads all images in a specified directory and creates a slide show
+ * Author: Justin W. Hall
+ * http://www.justinwhall.com/directory-jquery-slider/
+ *
+ * License: GPL v3
+ */
+
 (function($){
    var directorySlider = function(element, options)
    {
@@ -36,65 +44,59 @@
 
        $(elem).css('overflow', 'hidden');
 
-	//   var filelist = [];
-	   var slides = [];
-
-	   
-	slideNumber = 1;
+       // Get slides
+       var slides = [],
+       slideNumber = 1;
 
        while(slideNumber <= config.numslides){
          slides.push('<img src="' + config.directory + config.filebase + slideNumber + '.' + config.extension + '" />');
          slideNumber++;
        }
-	   
-					// append slideshow
-				    // apply slide wrap 1st
-				   var slideWrap = $('<div class="' + elemId + '-slide-wrap"></div>');
-					   slideWrap.appendTo(elem);
 
-					// append slide and position absolutley
-				   $.each(slides, function(index, val) {
-					 $(val).css({
-					   position: 'absolute',
-					   top: 0,
-					   left: 0,
-					   width: config.width // ADDED THIS SO WE DON'T NEED TO HAVE ALL IMAGES WITH SAME HEIGHT & WIDTH
-					 }).appendTo(slideWrap);
-				   });
+       // append slideshow
+       // apply slide wrap 1st
+       var slideWrap = $('<div class="' + elemId + '-slide-wrap" ></div>');
+           slideWrap.appendTo(elem);
 
-				setInterval(function(){
-				   var firstSlide = elem.find('img:first-child'),
-					   lastSlide = elem.find('img:last-child');
-				   // Apply animation
-				   switch(config.animation){
+        // append slide and position absolutley
+       $.each(slides, function(index, val) {
+         $(val).css({
+           position: 'absolute',
+           top: 0,
+           left: 0,
+           width: config.width // ADDED THIS SO WE DON'T NEED TO HAVE ALL IMAGES WITH SAME HEIGHT & WIDTH
+         }).appendTo(slideWrap);
+       });
 
-					case 'fade':
-						$(lastSlide).animate({
-						  opacity: 0},
-						  config.speed, function() {
-						  $(this).insertBefore(firstSlide).css('opacity', 1);
-						});
-					break;
+    setInterval(function(){
+       var firstSlide = elem.find('img:first-child'),
+           lastSlide = elem.find('img:last-child');
+       // Apply animation
+       switch(config.animation){
 
-					case 'uncover':
-						lastSlide.animate({
-						  marginLeft: -$(this).width()},
-						  config.speed, function() {
-						  $(this).insertBefore(firstSlide).css('marginLeft', 0);
-						});
-						break;
-					default:
-						$(lastSlide).animate({
-						  opacity: 0},
-						  config.speed, function() {
-						  $(this).insertBefore(firstSlide).css('opacity', 1);
-						});
-				   }
-				}, config.timeout);
-			}
-		});
-	
-       
+        case 'fade':
+            $(lastSlide).animate({
+              opacity: 0},
+              config.speed, function() {
+              $(this).insertBefore(firstSlide).css('opacity', 1);
+            });
+        break;
+
+        case 'uncover':
+            lastSlide.animate({
+              marginLeft: -$(this).width()},
+              config.speed, function() {
+              $(this).insertBefore(firstSlide).css('marginLeft', 0);
+            });
+            break;
+        default:
+            $(lastSlide).animate({
+              opacity: 0},
+              config.speed, function() {
+              $(this).insertBefore(firstSlide).css('opacity', 1);
+            });
+       }
+    }, config.timeout);
 
    };
 
@@ -116,3 +118,4 @@
        });
    };
 })(jQuery);
+
